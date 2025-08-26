fe0b93dfa1b5028233d1c0166ba986ff159f2285
jagdish gupta <varshneyjacky@gmail.com>
2025-08-26 10:24:43 +0530

    running ngmodule project

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,33 +1,41 @@
 import { NgModule, Component } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Course } from '../../../First_app/src/app/interfaces/course';
+import { Courses } from './pages/courses/courses';
+import { Admin } from './pages/admin/admin';
+import { HomeModule } from './pages/home/home-module';
 
 
 const routes: Routes = [
   // {path:'', redirectTo: '/home',pathMatch: 'full'},  or use below to redirect
   // {path:'home', component:Home}
-  // {path:'', component:Home},
-  //  {path:'about', component:About},
+   {path:'', component:Home},
+   {path:'about', component:About},
+   {path:'course', component:Courses},
+   {path:'admin', component:Admin},
 
-   {
-    path:' ',
-    loadChildren:()=>
-      import('./pages/home/home').then((m)=>m.Home),
-   },
-   {
-    path:'about',
-    loadChildren:()=>
-      import('./pages/about/about').then((m)=>m.About),
-   },
-   {
-    path:'admin',
-    loadChildren:()=>
-      import('./pages/admin/admin').then((m)=>m.Admin),
-   },
-   {
-    path:'courses',
-    loadChildren:()=>
-      import('./pages/courses/courses').then((m)=>m.Courses),
-   }
+  //  {
+  //   path:'',
+  //   loadChildren: () =>
+  //     import('./pages/home/home-module').then((m) => m.HomeModule),
+  //  },
+  //  {
+  //   path:'about',
+  //   loadChildren:()=>
+  //     import('./pages/about/about-module').then((m)=>m.AboutModule),
+  //  },
+  //  {
+  //   path:'admin',
+  //   loadChildren:()=>
+  //     import('./pages/admin/admin-module').then((m)=>m.AdminModule),
+  //  },
+  //  {
+  //   path:'courses',
+  //   loadChildren:()=>
+  //     import('./pages/courses/course-module').then((m)=>m.CourseModule),
+  //  }
   
 ];
 
